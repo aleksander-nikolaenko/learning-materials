4c918f0b8ef6814b4c01f4b8949a5a9c2a2772bd
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-01-29 20:26:42 +0200

    add task

---
+++ js/index.js
@@ -0,0 +1,109 @@
+/**
+ * Потрібно створити функцію яка буде приймати 1 параметр(масив юзерів) і серед них шукати Нарциса і повертати його імя(стрінгою) якщо в масиві немає Нарциса функція повертає пусту стрінгу, умова заключається в тому що НАРЦИС не знає нікого НАРЦИСА знають ВСІ
+ */
+
+
+const people1 = [{
+        name: 'Alex',
+        know: ['Alex', 'Jhon'],
+    },
+    {
+        name: 'Jhon',
+        know: []
+  },
+    {
+        name: 'Eva',
+        know: ['Alex', 'Jhon']
+    },
+    {
+        name: 'Ivan',
+        know: ['Jhon', 'Eva']
+    },
+]; 
+//нарцис  'Jhon'
+
+
+const people2 = [{
+        name: 'Alex',
+        know: ['Alex', 'Jhon'],
+    },
+    {
+        name: 'Jhon',
+        know: []
+  },
+    {
+        name: 'Eva',
+        know: []
+    },
+    {
+        name: 'Ivan',
+        know: ['Jhon', 'Eva']
+    },
+]; 
+//немає нарциса'
+
+
+const people3 = [{
+        name: 'Alex',
+        know: ['Alex','Eva'],
+    },
+    {
+        name: 'Jhon',
+        know: []
+  },
+    {
+        name: 'Eva',
+        know: ['Alex', 'Jhon']
+    },
+    {
+        name: 'Ivan',
+        know: ['Jhon', 'Eva']
+    },
+]; 
+//немає нарциса
+
+
+const people4 = [{
+        name: 'Alex',
+        know: ['Alex', 'Jhon'],
+    },
+    {
+        name: 'Jhon',
+        know: ['Eva']
+  },
+    {
+        name: 'Eva',
+        know: ['Alex', 'Jhon']
+    },
+    {
+        name: 'Ivan',
+        know: ['Jhon', 'Eva']
+    },
+]; 
+//немає нарциса'
+
+console.log(findNarcissus(people1));
+console.log(findNarcissus(people2));
+console.log(findNarcissus(people3));
+console.log(findNarcissus(people4));
+
+function findNarcissus (arr) {
+  for (let i = 0; i < arr.length; i += 1){
+    if (arr[i].know.length === 0) {
+      for (let j = 0; j < arr.length; j += 1){
+        if (j === i) continue;
+        if (!arr[j].know.includes(arr[i].name)) {
+          return "no Narcissus"
+        }
+        else {
+          continue
+        }
+      }
+      return arr[i].name;
+    }
+    else {
+      continue;
+    }
+  }
+  return "no Narcissus"
+ } 
