2358ebbca84e4fb7df20e1b7254b84c2aa47a135
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-02-12 14:24:49 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,107 +3,143 @@
  */
 
 
-const people1 = [{
-        name: 'Alex',
-        know: ['Alex', 'Jhon'],
-    },
-    {
-        name: 'Jhon',
-        know: []
-  },
-    {
-        name: 'Eva',
-        know: ['Alex', 'Jhon']
-    },
-    {
-        name: 'Ivan',
-        know: ['Jhon', 'Eva']
-    },
-]; 
-//нарцис  'Jhon'
+// const people1 = [{
+//         name: 'Alex',
+//         know: ['Alex', 'Jhon'],
+//     },
+//     {
+//         name: 'Jhon',
+//         know: []
+//   },
+//     {
+//         name: 'Eva',
+//         know: ['Alex', 'Jhon']
+//     },
+//     {
+//         name: 'Ivan',
+//         know: ['Jhon', 'Eva']
+//     },
+// ]; 
+// //нарцис  'Jhon'
 
 
-const people2 = [{
-        name: 'Alex',
-        know: ['Alex', 'Jhon'],
-    },
-    {
-        name: 'Jhon',
-        know: []
-  },
-    {
-        name: 'Eva',
-        know: []
-    },
-    {
-        name: 'Ivan',
-        know: ['Jhon', 'Eva']
-    },
-]; 
-//немає нарциса'
+// const people2 = [{
+//         name: 'Alex',
+//         know: ['Alex', 'Jhon'],
+//     },
+//     {
+//         name: 'Jhon',
+//         know: []
+//   },
+//     {
+//         name: 'Eva',
+//         know: []
+//     },
+//     {
+//         name: 'Ivan',
+//         know: ['Jhon', 'Eva']
+//     },
+// ]; 
+// //немає нарциса'
 
 
-const people3 = [{
-        name: 'Alex',
-        know: ['Alex','Eva'],
-    },
-    {
-        name: 'Jhon',
-        know: []
-  },
-    {
-        name: 'Eva',
-        know: ['Alex', 'Jhon']
-    },
-    {
-        name: 'Ivan',
-        know: ['Jhon', 'Eva']
-    },
-]; 
-//немає нарциса
+// const people3 = [{
+//         name: 'Alex',
+//         know: ['Alex','Eva'],
+//     },
+//     {
+//         name: 'Jhon',
+//         know: []
+//   },
+//     {
+//         name: 'Eva',
+//         know: ['Alex', 'Jhon']
+//     },
+//     {
+//         name: 'Ivan',
+//         know: ['Jhon', 'Eva']
+//     },
+// ]; 
+// //немає нарциса
 
 
-const people4 = [{
-        name: 'Alex',
-        know: ['Alex', 'Jhon'],
-    },
-    {
-        name: 'Jhon',
-        know: ['Eva']
-  },
-    {
-        name: 'Eva',
-        know: ['Alex', 'Jhon']
-    },
-    {
-        name: 'Ivan',
-        know: ['Jhon', 'Eva']
-    },
-]; 
-//немає нарциса'
+// const people4 = [{
+//         name: 'Alex',
+//         know: ['Alex', 'Jhon'],
+//     },
+//     {
+//         name: 'Jhon',
+//         know: ['Eva']
+//   },
+//     {
+//         name: 'Eva',
+//         know: ['Alex', 'Jhon']
+//     },
+//     {
+//         name: 'Ivan',
+//         know: ['Jhon', 'Eva']
+//     },
+// ]; 
+// //немає нарциса'
 
-console.log(findNarcissus(people1));
-console.log(findNarcissus(people2));
-console.log(findNarcissus(people3));
-console.log(findNarcissus(people4));
+// let start = 0;
+// let stop = 0;
+// // // start = (new Date()).getTime();
+// start = performance.now();
 
-function findNarcissus (arr) {
-  for (let i = 0; i < arr.length; i += 1){
-    if (arr[i].know.length === 0) {
-      for (let j = 0; j < arr.length; j += 1){
-        if (j === i) continue;
-        if (!arr[j].know.includes(arr[i].name)) {
-          return "no Narcissus"
-        }
-        else {
-          continue
-        }
-      }
-      return arr[i].name;
-    }
-    else {
-      continue;
-    }
-  }
-  return "no Narcissus"
- } 
+// console.log(start);
+// console.log(findNarcissus(people1));
+
+// console.log(findNarcissus(people2));
+
+// console.log(findNarcissus(people3));
+
+// console.log(findNarcissus(people4));
+// // stop = (new Date()).getTime();
+// stop = performance.now();
+// console.log(stop);
+// console.log("duration=", stop - start );
+
+// function findNarcissus (arr) {
+//   for (let i = 0; i < arr.length; i += 1){
+//     if (arr[i].know.length === 0) {
+//       for (let j = 0; j < arr.length; j += 1){
+//         if (j === i) continue;
+//         if (!arr[j].know.includes(arr[i].name)) {
+//           return "no Narcissus"
+//         }
+//         else {
+//           continue
+//         }
+//       }
+//       return arr[i].name;
+//     }
+//     else {
+//       continue;
+//     }
+//   }
+//   return "no Narcissus"
+// } 
+
+// start = performance.now();
+// console.log(findNar(people1));
+// console.log(findNar(people2));
+// console.log(findNar(people3));
+// console.log(findNar(people4));
+
+// stop = performance.now();
+// console.log("duration=", stop - start );
+ 
+// function findNar(arr) {
+//   const maybeNar = arr.filter(item => !item.know.length)
+
+//   if (maybeNar.length !== 1) {
+//     return '';
+//   }
+
+//   const name = maybeNar[0].name
+//   const response = arr.filter(item => item.know.length).every(item => item.know.includes(name))
+
+//   return response ? name : ''
+
+// }
