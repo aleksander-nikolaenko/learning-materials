66ab8511dfa32512b7439b4f001011e08ddea462
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-02-16 13:17:44 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
+ /**
+  * change the array so that elements with unique ones remain and add their qty 
+  */
+
+// const products = [{
+//   id: 'sku1',
+//   qty: 1,
+// }, {
+//   id: 'sku2',
+//   qty: 2,
+// }, {
+//   id: 'sku3',
+//   qty: 3,
+// }, {
+//   id: 'sku1',
+//   qty: 6,
+// }, {
+//   id: 'sku1',
+//   qty: 8,
+// }, {
+//   id: 'sku2',
+//   qty: 19,
+// }, {
+//   id: 'sku4',
+//   qty: 1,
+// }];
+
+// for (let i = 0; i < products.length; i += 1){
+//   for (let j = i + 1; j < products.length; j += 1){
+//     if (products[i].id === products[j].id) {
+//       products[i].qty += products[j].qty;
+//       products.splice(j, 1);
+//       j -= 1;
+//     }
+//   }
+// }
+
+// console.log(products);
+
+
 /**
  * Потрібно створити функцію яка буде приймати 1 параметр(масив юзерів) і серед них шукати Нарциса і повертати його імя(стрінгою) якщо в масиві немає Нарциса функція повертає пусту стрінгу, умова заключається в тому що НАРЦИС не знає нікого НАРЦИСА знають ВСІ
  */
@@ -19,7 +59,7 @@
 //         name: 'Ivan',
 //         know: ['Jhon', 'Eva']
 //     },
-// ]; 
+// ];
 // //нарцис  'Jhon'
 
 
@@ -39,7 +79,7 @@
 //         name: 'Ivan',
 //         know: ['Jhon', 'Eva']
 //     },
-// ]; 
+// ];
 // //немає нарциса'
 
 
@@ -59,7 +99,7 @@
 //         name: 'Ivan',
 //         know: ['Jhon', 'Eva']
 //     },
-// ]; 
+// ];
 // //немає нарциса
 
 
@@ -79,7 +119,7 @@
 //         name: 'Ivan',
 //         know: ['Jhon', 'Eva']
 //     },
-// ]; 
+// ];
 // //немає нарциса'
 
 // let start = 0;
@@ -100,6 +140,8 @@
 // console.log(stop);
 // console.log("duration=", stop - start );
 
+// первый вариант решения
+
 // function findNarcissus (arr) {
 //   for (let i = 0; i < arr.length; i += 1){
 //     if (arr[i].know.length === 0) {
@@ -119,7 +161,7 @@
 //     }
 //   }
 //   return "no Narcissus"
-// } 
+// }
 
 // start = performance.now();
 // console.log(findNar(people1));
@@ -130,6 +172,8 @@
 // stop = performance.now();
 // console.log("duration=", stop - start );
  
+/// второй вариант решения
+
 // function findNar(arr) {
 //   const maybeNar = arr.filter(item => !item.know.length)
 
@@ -143,3 +187,5 @@
 //   return response ? name : ''
 
 // }
+
+
